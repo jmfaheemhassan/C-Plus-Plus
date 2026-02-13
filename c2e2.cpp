f623351ed646337a6f189fb23767c84f2c2fa4d8
jmfahimhasan <pu66cse1079@primeuniversity.edu.bd>
2026-02-13 13:54:37 +0600

    chapter 2 exercise 2

---
+++ c2e2.cpp
@@ -0,0 +1,13 @@
+//  S.Prata C++ Primer Plus 6th ed.
+//  Programming Exercises 2.2 
+
+#include <iostream>
+int converts(int f){ return f*220; }
+int main()
+{
+    std::cout << "Enter furlongs: ";
+    int fur;
+    std::cin >> fur;
+    std::cout << fur << " furlongs = "<< converts(fur) << " yards" << std::endl;
+    return 0;
+}
