adf0953b9a273e5fc9c87b05ba1deca8bf76f941
jmfahimhasan <pu66cse1079@primeuniversity.edu.bd>
2026-02-13 13:07:54 +0600

    chapter 2 Exercies 1

---
+++ c2e1.cpp
@@ -0,0 +1,12 @@
+//  --------------------- S.Prata C++ Primer Plus 6th ed. ----------------------
+//  ------------------------ Programming Exercises 2.1 -------------------------
+
+#include <iostream>
+int main()
+{
+    std::cout << " Name: X\n Address: Italy, Rome" << std::endl;
+
+    std::cin.get(); //এইটা ব্যবহার করা হয়েছে যাতে প্রোগ্রামটি বন্ধ না হয়ে যায়, এটি ব্যবহারকারীকে একটি কী প্রেস করতে বলে 
+                      //তার পর প্রোগ্রাম বন্ধ হয়
+    return 0;
+}
